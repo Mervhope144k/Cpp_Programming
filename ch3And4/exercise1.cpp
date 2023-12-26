1a6faae1ca8484219db13bb9e5c75aab118dad39
Merveilles <merveilleskatumba@gmail.com>
2023-12-25 23:40:02 -0500

    chapter2 exercise1 and solution

---
+++ ch3And4/exercise1.cpp
@@ -0,0 +1,14 @@
+//Program to calculate the square root of 3 numbers
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+	cout << "Number" << "		" << "Square Root" << endl;
+	cout << " 4" << "		" << sqrt(4) << endl;
+	cout << "12.25" << "		" << sqrt(12.25) << endl;
+	cout << " 0.0121" << "		" << sqrt(0.0121) << endl;
+
+	return 0;
+}
